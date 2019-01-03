25088781ceecc3c1b73c99403d7fc383c1d3a92e
Glen Buck <glenbuck503@gmail.com>
2019-01-03 11:07:28 -0800

    Added Step 103

---
+++ ExercisePage103/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        //string[] stringArray = { "This is One", "This is Two", "This is Three", "This is Four" };
+        //Console.WriteLine("Please enter some text");
+        //string stringInput = Console.ReadLine();
+        //for (int i = 0; i < stringArray.Length; i++)
+        //{
+        //    if (i < stringArray.Length)
+        //    {
+        //        Console.WriteLine(stringInput + stringArray[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        //QUESTIONS 2 AND 3
+        //Infinite Loop as read below. To fix it you would add "++" after the J to line 30 to make it sringNum2[j++] instead of stringNum2[j]
+
+        //string[] stringNum2 = { "This is One", "This is Two", "This is Three", "This is Four" };
+        //Console.WriteLine("Please enter some text");
+        //string stringInput2 = Console.ReadLine();
+        //for (int j = 0; j < stringNum2.Length;)
+        ////to correct the infinite loop you would add j++ after the .length;
+        //{
+        //    if (j < stringNum2.Length)
+        //    {
+        //        Console.WriteLine(stringInput2 + stringNum2[j]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        // QUESTIONS 4 AND  for step 5,to create a loop that uses <= , replace operators
+
+        //int[] testScroes = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+        //Console.WriteLine("enter a number");
+        //string input = Console.ReadLine();
+        //int userInput = Convert.ToInt32(input);
+
+        //for (int k = 0; i < testScroes.Length; i++)
+        //{
+        //    if (testScroes[k] < 80)
+        //    
+        //    {
+        //        Console.WriteLine("passing:" + testScroes[k++]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        //QUESTION 6,7 and 8
+
+        //List<string> newString = new List<string>() { "Brown", "White", "Black" };
+        //Console.WriteLine("Please choose a color between: Brown, White or Black:");
+        //string userChoiceColor = Console.ReadLine();
+        //bool found = false;
+
+        //for (int m = 0; m < newString.Count; m++)
+        //{
+        //    if (userChoiceColor == newString[m])
+        //    {
+        //        Console.WriteLine(m);
+        //        found = true;
+        //        break;
+        //    }
+        //}
+        //if (found == false)
+        //{
+        //    Console.WriteLine("You have entered a choice that is not an option");
+        //}
+        //Console.ReadLine();
+
+
+        //QUESTION 9 and 10
+
+        //List<string> newString1 = new List<string>() { "One", "Two", "Three", "Four", "Two" };
+        //Console.WriteLine("Please choose a number between One and Four:");
+        //string userChoiceNumber = Console.ReadLine();
+        //bool found1 = false;
+
+        //for (int n = 0; n < newString1.Count; n++)
+        //{
+        //    if (userChoiceNumber == newString1[n])
+        //    {
+        //        Console.WriteLine(n);
+        //        found1 = true;   
+        //    }
+
+        //}
+        //if (found1 == false)
+        //{
+        //    Console.WriteLine("You have entered a choice that is not an option");
+        //}
+        //Console.ReadLine();
+
+
+        //QUESTION 11
+
+        //List<string> newString2 = new List<string>() { "This", "Is", "Question", "Number", "Eleven", "Question" };
+        //List<string> newString3 = new List<string>();
+      
+        //foreach (string item in newString2)
+        //{
+        //    if (newString3.Contains(item))
+        //    {
+        //        Console.WriteLine(item + " has already appeared in the list.");
+        //    }
+        //    else
+        //    {
+        //        newString3.Add(item);
+        //        Console.WriteLine(item);
+        //    }
+        //}
+        //Console.ReadLine();
+    }
+}
+
+
